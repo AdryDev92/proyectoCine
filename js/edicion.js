f12a231f1ceb75e668f2847180e510124a052ee1
AdryDev92 <tazzmaniac51@gmail.com>
2017-11-03 20:21:30 +0100

    método de validación actualizado

--- js/edicion.js
+++ js/edicion.js
@@ -4,19 +4,7 @@
  */
 
 
-let arrayPeliculas = [
-    new Pelicula("matrix",["ciencia_ficcion"],"Lilly Wachowski","Keanu Reeves","1999","junio","Muy Buena","60 min","5"),
-    new Pelicula("matrix reloaded",["ciencia_ficcion"],"Lilly Wachowski","Keanu Reeves","2001","junio","Muy Buena","120 min","5"),
-    new Pelicula("el francotirador",["accion"],"Clint Eastwood","Bradley Cooper", "2014","febrero","Muy buena","90 min","4"),
-    new Pelicula("blade",["comedia"],"Stephen Norrington","Wesley Snipes","1998","mayo","Normalita","60 min","3"),
-    new Pelicula("hulk",["romantica"],"Jack Kirby","Mark Ruffalo","1962","octubre","Mala","180 min","5"),
-    new Pelicula("toc toc",["comedia"],"Vicente Villanueva","Paco León","2017","octubre","Muy buena","96 min","9"),
-    new Pelicula("thor ragnarok",["ciencia ficcion"],"Taika Waititi","Chris Hemsworth","2017","octubre","Buena","130 min","8"),
-    new Pelicula("blade runner",["ciencia ficcion"],"Denis Villeneuve","Ryan Gosling","2017","octubre","Buena","163 min","5"),
-    new Pelicula("it",["miedo"],"Andrés Muschietti","Sophia Lilis","2017","septiembre","normal","135 min","5")
-];
-
-function validacion(arrayPeliculas) {
+function validacion(nombre,genero,descripcion, director, actores,duracion,anio,mes,nombre_actor,nombre_director) {
 
 
     //esta parte recoge los datos directamente de los inputs
@@ -32,11 +20,18 @@ function validacion(arrayPeliculas) {
     let nombre_actor = document.getElementById('nombre_actor').value;
     let nombre_director = document.getElementById('nombre_director').value;*/
 
-    for(let i = 0; i<arrayPeliculas.length;i++){
+  validacion(nombre);
+  validacion(genero);
+  validacion(descripcion);
+  validacion(director);
+  validacion(actores);
+  validacion(duracion);
+  validacion(anio);
+  validacion(mes);
+  validacion(nombre_actor);
+  validacion(nombre_director);
 
-        validacion.some(arrayPeliculas[i]);
-
-        /*if(  nombre
+        if(  nombre
             && genero
             && descripcion
             && director
@@ -48,10 +43,11 @@ function validacion(arrayPeliculas) {
             && nombre_director)
         {
             console.log("TODO OK");
-        }*/
+        }
 
     }
 
+validacion("Matrix","ciencia ficcion","una breve descripcion","director_X","actores",120,2017,"Julio","Matt Daemon","Un director");
 
 }
 
