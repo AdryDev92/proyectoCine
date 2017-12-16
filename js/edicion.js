99f2373266437c8cf797360a1fbbf84f9abc6455
AdryDev92 <tazzmaniac51@gmail.com>
2017-12-16 23:17:59 +0100

    Add files via upload

--- js/edicion.js
+++ js/edicion.js
@@ -1,35 +1,11 @@
-/*
- Valida los campos de una pelicula,
- asegurandose de que no puedan ser nulos, vacios o undefined.
- */
 
 
-function validacion(nombre,genero,descripcion, director, actores,duracion,anio,mes,nombre_actor,nombre_director) {
+function validacion(arrayPeliculas) {
 
 
-    //esta parte recoge los datos directamente de los inputs
+    for(let i = 0; i<arrayPeliculas.length;i++){
 
-    /*let nombre = document.getElementById('nombre').value;
-    let genero = document.getElementById('genero').value;
-    let descripcion = document.getElementById('descripcion').value;
-    let director = document.getElementById('director').value;
-    let actores = document.getElementById('actores').value;
-    let duracion = document.getElementById('duracion').value;
-    let anio = document.getElementById('anio').value;
-    let mes = document.getElementById('mes').value;
-    let nombre_actor = document.getElementById('nombre_actor').value;
-    let nombre_director = document.getElementById('nombre_director').value;*/
-
-  validacion(nombre);
-  validacion(genero);
-  validacion(descripcion);
-  validacion(director);
-  validacion(actores);
-  validacion(duracion);
-  validacion(anio);
-  validacion(mes);
-  validacion(nombre_actor);
-  validacion(nombre_director);
+        validacion.some(arrayPeliculas[i]);
 
         if(  nombre
             && genero
@@ -42,14 +18,15 @@ function validacion(nombre,genero,descripcion, director, actores,duracion,anio,m
             && nombre_actor
             && nombre_director)
         {
-            console.log("TODO OK");
+            console.log("OK");
         }
 
     }
 
-validacion("Matrix","ciencia ficcion","una breve descripcion","director_X","actores",120,2017,"Julio","Matt Daemon","Un director");
-
 }
+
+validacion("Matrix","ciencia_ficcion","Lilly Wachowski","junio","1 hora",2016,8,"Silvester Stalone","Matt Daemon");
+
 
 
 function addUpdateActor(nombre_actor) {
@@ -61,7 +38,6 @@ function addUpdateActor(nombre_actor) {
 
     console.log(peliculas);
 
-
 }
 
 
@@ -70,7 +46,7 @@ function addUpdateDirector(nombre_director) {
     let peliculas = [];
 
         let director = peliculas.indexOf(nombre_director);
-        if( director == -1 ){
+        if( director === -1 ){
             peliculas.push(director);
         }else{
             peliculas[director] = nombre_director;
